3c52c047033cde8641e06d1542cc2966e082915d
Aliylmaz <111225614+Aliylmaz@users.noreply.github.com>
2025-07-16 21:42:55 +0300

    exceptions and handler added

---
+++ insurance-app/src/main/java/com/ada/insurance_app/handler/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.ada.insurance_app.handler;
+
+import com.ada.insurance_app.exception.InvalidPasswordException;
+import com.ada.insurance_app.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidPasswordException.class)
+        public ResponseEntity<Object> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+
+
+
+
+    private ResponseEntity<Object> buildErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> errorDetails = new LinkedHashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", status.value());
+        errorDetails.put("error", status.getReasonPhrase());
+        errorDetails.put("message", message);
+
+        return new ResponseEntity<>(errorDetails, status);
+    }
+}
