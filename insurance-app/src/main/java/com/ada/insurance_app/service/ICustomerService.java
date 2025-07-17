55244ab2865d8c880f165efa06fef44c4207798c
Aliylmaz <111225614+Aliylmaz@users.noreply.github.com>
2025-07-17 11:05:50 +0300

    Service interface and implementation classes Added

---
+++ insurance-app/src/main/java/com/ada/insurance_app/service/ICustomerService.java
@@ -0,0 +1,4 @@
+package com.ada.insurance_app.service;
+
+public interface ICustomerService {
+}
---
+++ insurance-app/src/main/java/com/ada/insurance_app/service/Impl/OfferServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ada.insurance_app.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferServiceImpl {
+}
